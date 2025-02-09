4f6048215c0001c476d5c1079d735c6d2e67b613
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-02-09 11:08:25 +0500

    refactor images url

--- src/entities/product/ui/card.tsx
+++ src/entities/product/ui/card.tsx
@@ -13,7 +13,7 @@ const ProductCard: FC<Props> = ( { id, name, price, discount = 0, images } ) => 
   return (
     <article itemScope itemType="https://schema.org/Product" className="text-xs">
       <Link to={id}>
-        <img itemProp="image" src={`${import.meta.env.VITE_REACT_APP_BACK_URL}/uploads/${images?.[0]}.webp`} alt={name} className="w-full aspect-[6/7] bg-zinc-200 object-cover" />
+        <img itemProp="image" src={`${import.meta.env.VITE_REACT_APP_BLOB_URL}/${images?.[0]}.webp`} alt={name} className="w-full aspect-[6/7] bg-zinc-200 object-cover" />
       </Link>
       <meta itemProp="name" content={name} />
       <meta itemProp="priceCurrency" content="USD" />
